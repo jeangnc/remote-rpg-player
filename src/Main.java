6e170f572a742e722ded26c8b4637a41a678c81f
Jean Gonçalves <jean.gnc@gmail.com>
2018-11-18 16:59:53 -0200

    Running initial version

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        AtorNetGames rede = new AtorNetGames();
+
+        rede.conectar("localhost", "Jean");
+
+        System.out.println("Hello World!");
+    }
+}
