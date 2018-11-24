83a1e08aaf5bf935068b037d1399f9a4f4da3907
Jean Gonçalves <jean.gnc@gmail.com>
2018-11-24 21:23:49 -0200

    Listen map click event

--- src/interface_grafica/VisualizacaoMapa.java
+++ src/interface_grafica/VisualizacaoMapa.java
@@ -5,6 +5,8 @@ import modelos.Mapa;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 class VisualizacaoMapa extends JPanel {
 
@@ -27,8 +29,20 @@ class VisualizacaoMapa extends JPanel {
         for (int i = 0; i < mapa.retornarLargura(); i++) {
             for (int j = 0; j < mapa.retornarAltura(); j++) {
                 Border border = BorderFactory.createLineBorder(Color.RED, 1);
-                JLabel label = new JLabel(i + " - " + j);
+                JLabel label = new JLabel();
+                label.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                 label.setBorder(border);
+
+                int finalI = i;
+                int finalJ = j;
+                label.addMouseListener(new MouseAdapter()  {
+                    @Override
+                    public void mouseClicked(MouseEvent e)  {
+                        System.out.println(finalI + " - " + finalJ);
+
+                    }
+                });
+
                 p.add(label);
             }
         }
