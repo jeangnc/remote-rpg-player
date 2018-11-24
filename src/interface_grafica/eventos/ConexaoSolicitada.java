12b6813bd72f91f5013907b0550403961d5594ad
Jean Gonçalves <jean.gnc@gmail.com>
2018-11-24 20:07:42 -0200

    Implement connection request event

--- src/interface_grafica/eventos/ConexaoSolicitada.java
+++ src/interface_grafica/eventos/ConexaoSolicitada.java
@@ -1,10 +1,19 @@
 package interface_grafica.eventos;
 
 public class ConexaoSolicitada extends EventoInterface {
+    private int id;
+    private String nome;
 
-    public ConexaoSolicitada() {
-        // TODO - implement ConexaoSolicitada.ConexaoSolicitada
-        throw new UnsupportedOperationException();
+    public ConexaoSolicitada(int i, String n) {
+        id = i;
+        nome = n;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
--- src/interface_grafica/eventos/EventoInterface.java
+++ src/interface_grafica/eventos/EventoInterface.java
@@ -2,5 +2,5 @@ package interface_grafica.eventos;
 
 import comunicacao.Evento;
 
-public abstract class EventoInterface extends InicioSolicitado implements Evento {
+public abstract class EventoInterface implements Evento {
 }
