2e9dd064e5a07b3e31d5534b9ce1630b4d51bb79
Jean Gonçalves <jean.gnc@gmail.com>
2018-11-26 14:56:11 -0200

    Add waiting players' view

---
+++ src/interface_grafica/visualizacoes/AguardandoJogadores.java
@@ -0,0 +1,13 @@
+package interface_grafica.visualizacoes;
+
+import interface_grafica.Visualizacao;
+
+import javax.swing.*;
+
+public class AguardandoJogadores extends Visualizacao {
+    private JPanel panel;
+
+    public JPanel renderizar() {
+        return panel;
+    }
+}
